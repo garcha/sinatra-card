e318c34011a171f5673d655e9d163debc0e1d1e3
Jaspreet Garcha <jaspreet@garcha.com>
2014-03-06 13:15:00 -0800

    file updates

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require './environments'
 
 class Card < ActiveRecord::Base
 end
+
+get "/" do
+  @cards = Card.order("created_at DESC")
+  @title = "Welcome"
+  erb :"cards/index"
+end
+
+helpers do
+  def title
+    if @title
+      "#@{title}"
+    else
+      "Welcome"
+    end
+  end
+end
